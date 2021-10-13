190213f99d0f4e556192a09132e77c8614d8c820
LYshiying <839778960@qq.com>
2021-10-13 18:09:38 +0800

    :tada: 0.0.2更新！增加了license！

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup
 
-# TODO(shiying): 去找个愿意帮忙写README的苦工，奥！苦工竟是我自己！
-# with open("./soraha_utils/README.md", "r") as f:
-#    long_des = f.read()
+# TODO(shiying): 去找个愿意帮忙写README的苦工
+# 2021/10/13 18:02 GMT+8 编辑: 奥！苦工竟是我自己！
+with open("./README.md", "r", encoding="utf-8") as f:
+    long_des = f.read()
 
 setup(
     name="soraha_utils",
-    version="0.0.1",
+    version="0.0.2",
     author="shiying",
     author_email="839778960@qq.com",
     description="soraha_utils",
-    long_description="是羽衣在杂物间辛苦一天一夜找出来的各种小东西！请好好的使用吧！不要再随意丢弃了！(未完待续……预计0.0.2将会更新README.md)",
+    long_description=long_des,
     long_description_content_type="text/markdown",
     url="https://www.github.com/LYshiying/soraha_utils",
     packages=["soraha_utils"],
--- soraha_utils/__init__.py
+++ soraha_utils/__init__.py
@@ -13,7 +13,7 @@ __all__ = [
     "sync_to_async",
     "logger",
 ]
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 with_hook = hook_manager().with_hook
 logger = set_logger()
